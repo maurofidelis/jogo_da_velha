88f3db4481f7b2f783d9866c957732cad086edf4
Mauro Pontes <37085690+maurofidelis@users.noreply.github.com>
2020-06-30 19:37:55 -0300

    add regras, rm imprime tabuleiro

--- jogo_velha/jogo_velha.py
+++ jogo_velha/jogo_velha.py
@@ -1,4 +1,5 @@
 tabuleiro = [['*', 'A', 'B', 'C'], [1, ' ', ' ', ' '], [2, ' ', ' ', ' '], [3, ' ', ' ', ' ']]
+dicionario = {'A' : 1, 'B' : 2, 'C' : 3}
 
 def tabuleiro_vazio():
     print('-' * 30)
@@ -8,25 +9,24 @@ def tabuleiro_vazio():
         print()
     print('-' * 30)
 
-def imprime_tabuleiro (lin, col, peca):
+def regras (linha, coluna, peca):
+    """Define as regras do jogo, retornando false caso a jogada esteja contra as regras
+    >>> Selecione a jogada: a0
+    False
     """
-    Imprime o tabuleiro e vai atualizando a cada jogada
-    """
-    print('-' * 30)
-    for linha in range(0, 4):
-        for coluna in range(0, 4):
-            # Definindo a coluna que a peça vai ser posicionada
-            if tabuleiro[linha][coluna] == col:
-                pos_col = coluna
-            #Posicionando a peça
-            if linha == int(lin) and coluna == pos_col:
-                tabuleiro[linha][coluna] = peca
-                print(f'[{tabuleiro[linha][coluna]}] | ', end=' ')
-            #Se não estiver na posição da peça continua imprimindo o tabuleiro
-            else:
-                print(f'[{tabuleiro[linha][coluna]}] | ', end=' ')
-        print()
-    print('-' * 30)
+    #Coluna informada é inválida
+    if coluna != 'A' and coluna != 'B' and coluna != 'C':
+        return False     
+    #linha informada é inválida
+    elif linha != '1' and linha != '2' and linha != '3':
+        return False
+    #posição já tem uma peça
+    elif tabuleiro[int(linha)][dicionario[coluna]] != ' ':
+        return False
+    else:
+        #preenche tabuleiro coma peça passada
+        tabuleiro[int(linha)][dicionario[coluna]] = peca
+        return True
 
 def main():
     #Menu
@@ -34,7 +34,6 @@ def main():
 
     #Tabuleiro Vazio
     tabuleiro_vazio()
-
     cont = 1
     while (True):
         if cont % 2 == 0:
@@ -44,13 +43,15 @@ def main():
         jogada =(input('Seleciona a jogada: ')).upper()
         lin = jogada[1]
         col = jogada[0]
-        imprime_tabuleiro(lin, col, peca)
-        # Definir condição de parada
-        cont = cont + 1
-
+        if (regras(lin, col, peca)):
+            tabuleiro_vazio()
+            #O contador define quando é X ou O e so é atualizado qdo a jogada for válida
+            cont = cont + 1
+        else:
+            print("Jogada Inválida")
+       
 main()
     
-
 
         
 
