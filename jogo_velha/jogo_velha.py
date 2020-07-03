e184465112c1e6f5188413e3ef22cde6b6f1fee2
Mauro Pontes <37085690+maurofidelis@users.noreply.github.com>
2020-07-03 14:35:17 -0300

    add verifica_vitoria

--- jogo_velha/jogo_velha.py
+++ jogo_velha/jogo_velha.py
@@ -1,7 +1,11 @@
-tabuleiro = [['*', 'A', 'B', 'C'], [1, ' ', ' ', ' '], [2, ' ', ' ', ' '], [3, ' ', ' ', ' ']]
+tabuleiro = [['*', 'A', 'B', 'C'], ['1', ' ', ' ', ' '], ['2', ' ', ' ', ' '], ['3', ' ', ' ', ' ']]
 dicionario = {'A' : 1, 'B' : 2, 'C' : 3}
+vetor_X = ['X', 'X', 'X']
+vetor_Y = ['O', 'O', 'O']
 
-def tabuleiro_vazio():
+def imprime_tabuleiro():
+    """Imprime o tabuleiro 
+    """
     print('-' * 30)
     for linha in range(0, 4):
         for coluna in range(0,4):
@@ -11,6 +15,7 @@ def tabuleiro_vazio():
 
 def regras (linha, coluna, peca):
     """Define as regras do jogo, retornando false caso a jogada esteja contra as regras
+       Atualiza o tabuleiro poscionando a peça
     >>> Selecione a jogada: a0
     False
     """
@@ -28,12 +33,51 @@ def regras (linha, coluna, peca):
         tabuleiro[int(linha)][dicionario[coluna]] = peca
         return True
 
+def verifica_vitoria():
+    #verificando linha e coluna
+    for linha in range(1, 4):
+        vetor_aux = [' ', ' ', ' ']
+        vetor_aux_coluna = [' ', ' ', ' ']
+        for coluna in range(1, 4):
+            vetor_aux[coluna-1] = tabuleiro[linha][coluna]
+            vetor_aux_coluna[coluna-1] = tabuleiro[coluna][linha]
+        if vetor_aux == vetor_X or vetor_aux == vetor_Y:
+            print("Vitória!")
+            return True
+        if vetor_aux_coluna == vetor_X or vetor_aux_coluna == vetor_Y:
+            print("Vitória!")
+            return True
+    #verificando diagonais
+    if tabuleiro[1][1] == tabuleiro[2][2] == tabuleiro[3][3] != ' ':
+        print("Vitória!")
+        return True
+    if tabuleiro[1][3] == tabuleiro[2][2] == tabuleiro[3][1] != ' ':
+        print("Vitória!")
+        return True
+    
 def main():
     #Menu
-
+    '''
+    print('*' * 30)
+    print('0 - Informações')
+    print('1 - Novo Jogo')
+    print('2 - Sair')
+    opc = input('Selecione a opção desejada: ')    
+    print('*' * 30)
+    #Condições do menu
+    if opc == 0:
+        infomacoes()
+    elif opc == 1:
+        game()
+    elif opc == 2:
+        exit()
+    else:
+        print("Comando Inválido")
+        return False
+    '''
 
     #Tabuleiro Vazio
-    tabuleiro_vazio()
+    imprime_tabuleiro()
     cont = 1
     while (True):
         if cont % 2 == 0:
@@ -44,13 +88,16 @@ def main():
         lin = jogada[1]
         col = jogada[0]
         if (regras(lin, col, peca)):
-            tabuleiro_vazio()
+            imprime_tabuleiro()
             #O contador define quando é X ou O e so é atualizado qdo a jogada for válida
+            if (verifica_vitoria()):
+                return False
             cont = cont + 1
         else:
             print("Jogada Inválida")
        
 main()
+
     
 
         
